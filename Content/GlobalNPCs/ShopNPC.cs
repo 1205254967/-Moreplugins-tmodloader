270af9bf0bd4e1e564763595249ad9c6c339cc03
nanianjinxiu <liaojin20010212@163.com>
2026-03-03 02:05:33 +0800

    2.3

---
+++ Content/GlobalNPCs/ShopNPC.cs
@@ -0,0 +1,14 @@
+﻿using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria;
+using Terraria.Chat;
+
+namespace Moreplugins.Content.GlobalNPCs
+{
+    internal class ShopNPC : GlobalNPC
+    {
+        public override void NPCShop(int npcType, string name)
+        {
+        }
+    }
+}
