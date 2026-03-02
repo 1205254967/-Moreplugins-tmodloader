48ef5f9fb6906746140cc57715594cf6d5b79ab9
nanianjinxiu <liaojin20010212@163.com>
2026-03-03 03:37:13 +0800

    让雷管塞由炸弹商人处15金币购买，让恶魔眼1%概率掉落眼球塞，依旧没有做本地化

--- Content/GlobalNPCs/ShopNPC.cs
+++ Content/GlobalNPCs/ShopNPC.cs
@@ -2,13 +2,19 @@
 using Terraria.ModLoader;
 using Terraria;
 using Terraria.Chat;
+using Moreplugins.Content.Items.Accessories;
 
 namespace Moreplugins.Content.GlobalNPCs
 {
     internal class ShopNPC : GlobalNPC
     {
-        public override void NPCShop(int npcType, string name)
+        //在炸弹商人商店中贩卖雷管插件
+        public override void ModifyShop(NPCShop shop)
         {
+            if (shop.NpcType == NPCID.Demolitionist)
+            {
+                shop.Add<DetonatorPlugins>();
+            }
         }
     }
 }
