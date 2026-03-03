5ab876f152c67e021ed7d4e4fd777cde3b1877b8
nanianjinxiu <liaojin20010212@163.com>
2026-03-04 04:56:13 +0800

    附魔塞

--- Content/Items/Accessories/EnchantedPlusins.cs
+++ Content/Items/Accessories/EnchantedPlusins.cs
@@ -1,5 +1,10 @@
-﻿using Terraria;
+﻿using Moreplugins.Content.Players;
+using System.Collections.Generic;
+using System.Threading.Channels;
+using Terraria;
+using Terraria.Audio;
 using Terraria.ID;
+using Terraria.ModLoader;
 
 namespace Moreplugins.Content.Items.Accessories
 {
@@ -18,6 +23,89 @@ namespace Moreplugins.Content.Items.Accessories
         public override void UpdateAccessory(Player player, bool hideVisual)
         {
             base.UpdateAccessory(player, hideVisual);
+            int warding = 0;
+            int lucky = 0;
+            int menacing = 0;
+            int quick = 0;
+            int violent = 0;
+
+            for (int i = 0; i < player.armor.Length; i++)
+            {
+                Item acc = player.armor[i];
+                if (acc.prefix == PrefixID.Warding)
+                {
+                    warding += 2;
+                    // 这块加
+                    continue;
+                }
+                if (acc.prefix == PrefixID.Lucky)
+                {
+                    lucky += 2;
+                    continue;
+                }
+                if (acc.prefix == PrefixID.Menacing)
+                {
+                    menacing += 2;
+                    continue;
+                }
+                if (acc.prefix == PrefixID.Quick)
+                {
+                    quick += 2;
+                    continue;
+                }
+                if (acc.prefix == PrefixID.Violent)
+                {
+                    violent += 2;
+                    continue;
+                }
+            }
+            player.statDefense += warding;
+
+            ref float genericCrit = ref player.GetCritChance<GenericDamageClass>();
+            genericCrit += lucky;
+
+            ref StatModifier genericDamage = ref player.GetDamage<GenericDamageClass>();
+            genericDamage += menacing / 100f;
+
+            player.moveSpeed += quick / 100f;
+
+            ref float genericAttackSpeed = ref player.GetAttackSpeed<GenericDamageClass>();
+            genericAttackSpeed += lucky;
+        }
+
+        public override void ModifyTooltips(List<TooltipLine> tooltips)
+        {
+            Player player = Main.LocalPlayer;
+            for (int i = 0; i < player.armor.Length; i++)
+            {
+                Item acc = player.armor[i];
+                if (acc.prefix == PrefixID.Warding)
+                {
+
+                    // 这块加
+                    continue;
+                }
+                if (acc.prefix == PrefixID.Lucky)
+                {
+
+                    continue;
+                }
+                if (acc.prefix == PrefixID.Menacing)
+                {
+
+                    continue;
+                }
+                if (acc.prefix == PrefixID.Quick)
+                {
+
+                    continue;
+                }
+                if (acc.prefix == PrefixID.Violent)
+                {
+
+                    continue;
+                }
+            }
         }
     }
 }
